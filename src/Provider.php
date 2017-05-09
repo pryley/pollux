a9709b27b47a980c89b1cf935254188cd8b9c332
Paul Ryley <paul@geminilabs.io>
2017-05-09 11:08:48 -0400

    load ConfigManager as a singleton

--- src/Provider.php
+++ src/Provider.php
@@ -3,11 +3,13 @@
 namespace GeminiLabs\Pollux;
 
 use GeminiLabs\Pollux\Application;
+use GeminiLabs\Pollux\Config\ConfigManager;
 
 class Provider
 {
 	public function register( Application $app )
 	{
 		$app->bind( Application::class, $app );
+		$app->singleton( ConfigManager::class, ConfigManager::class );
 	}
 }
