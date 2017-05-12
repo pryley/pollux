4534a72e42c12d9bd506f7d71de3ef626d5793a3
Paul Ryley <paul@geminilabs.io>
2017-05-12 02:06:16 -0400

    major GateKeeper modifications (needs trimming!)

--- src/GateKeeper.php
+++ src/GateKeeper.php
@@ -2,10 +2,32 @@
 
 namespace GeminiLabs\Pollux;
 
+use Exception;
+use GeminiLabs\Pollux\Config\Config;
+use ReflectionClass;
+
 class GateKeeper
 {
+	/**
+	 * [plugin_file_path] => [plugin_name]|[plugin_version]|[plugin_url]
+	 */
+	const DEPENDENCIES = [
+		'meta-box/meta-box.php' => 'Meta Box|4.11|https://wordpress.org/plugins/meta-box/',
+	];
 	const MIN_PHP_VERSION = '5.6.0';
 	const MIN_WORDPRESS_VERSION = '4.7';
+
+	public $errors = [];
+
+	/**
+	 * @var Application
+	 */
+	protected $app;
+
+	/**
+	 * @var Notice
+	 */
+	protected $notice;
 
 	/**
 	 * @var string
@@ -14,29 +36,72 @@ class GateKeeper
 
 	public function __construct( $plugin )
 	{
-		if( $this->proceed() )return;
-
 		$this->plugin = $plugin;
 
-		add_action( 'activated_plugin', array( $this, 'deactivate' ));
-		add_action( 'admin_notices',    array( $this, 'deactivate' ));
+		if( $this->canActivate() ) {
+			add_action( 'admin_init', array( $this, 'init' ));
+		}
+		else {
+			add_action( 'activated_plugin', array( $this, 'deactivate' ));
+			add_action( 'admin_notices',    array( $this, 'deactivate' ));
+		}
+	}
+
+	public function init()
+	{
+		$this->app = pollux_app();
+		$this->notice = pollux_app()->make( 'Notice' );
+
+		add_action( 'wp_ajax_pollux/dependency/activate', array( $this, 'ajaxActivatePlugin' ));
+		add_action( 'wp_ajax_pollux/dependency/updated',  array( $this, 'ajaxIsPluginActive' ));
+		add_action( 'admin_notices',                      array( $this, 'printNotices' ));
+		add_action( 'current_screen',                     array( $this, 'setDependencyNotice' ));
+	}
+
+	/**
+	 * @return void
+	 */
+	public function ajaxIsPluginActive()
+	{
+		check_ajax_referer( 'updates' );
+		$plugin = filter_input( INPUT_POST, 'plugin' );
+		if( !$this->isPluginDependency( $plugin )) {
+			wp_send_json_error();
+		}
+		$status = [
+			'updated' => $plugin,
+			'pluginName' => $this->getPluginInformation( $plugin, 'name' ),
+		];
+		if( !$this->isPluginActive( $plugin )) {
+			$actionUrl = self_admin_url( sprintf( 'plugins.php?action=activate&plugin=%s', $plugin ));
+			$status['activateUrl'] = wp_nonce_url( $actionUrl, sprintf( 'activate-plugin_%s', $plugin ));
+		}
+		wp_send_json_success( $status );
+	}
+
+	/**
+	 * @return void
+	 */
+	public function ajaxActivatePlugin()
+	{
+		check_ajax_referer( 'updates' );
+		$plugin = filter_input( INPUT_POST, 'plugin' );
+		if( !$this->isPluginDependency( $plugin ) || is_wp_error( activate_plugin( $plugin, null, is_network_admin(), true ))) {
+			wp_send_json_error();
+		}
+		wp_send_json_success([
+			'activate' => $plugin,
+			'pluginName' => $this->getPluginInformation( $plugin, 'name' ),
+			'slug' => $this->getPluginSlug( $plugin ),
+		]);
 	}
 
 	/**
 	 * @return bool
 	 */
-	public function checkPhpVersion()
+	public function canActivate()
 	{
-		return version_compare( PHP_VERSION, self::MIN_PHP_VERSION, '>=' );
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function checkWordPressVersion()
-	{
-		global $wp_version;
-		return version_compare( $wp_version, self::MIN_WORDPRESS_VERSION, '>=' );
+		return $this->hasValidPHPVersion() && $this->hasValidWPVersion();
 	}
 
 	/**
@@ -50,35 +115,313 @@ class GateKeeper
 			$this->redirect();
 		}
 		deactivate_plugins( $this->plugin );
-		$this->printNotice();
+		$addNotice = $this->hasValidPHPVersion()
+			? 'addInvalidWPVersionNotice'
+			: 'addInvalidPHPVersionNotice';
+		$this->$addNotice();
 	}
 
 	/**
-	 * @return void
+	 * @return void|null
 	 */
-	public function printNotice()
+	public function getDependencyAction()
 	{
-		$message1 = $this->checkPhpVersion()
-			? sprintf( __( 'Sorry, Pollux requires WordPress %s or greater in order to work properly.', 'pollux' ), self::MIN_WORDPRESS_VERSION )
-			: sprintf( __( 'Sorry, Pollux requires PHP %s or greater in order to work properly (your server is running PHP %s).', 'pollux' ), self::MIN_PHP_VERSION, PHP_VERSION );
+		if( get_current_screen()->id != 'settings_page_pollux' )return;
 
-		$message2 = $this->checkPhpVersion()
-			? sprintf( '<a href="%s">%s</a>', admin_url( 'update-core.php' ), __( 'Update WordPress', 'pollux' ))
-			: __( 'Please contact your hosting provider or server administrator to upgrade the version of PHP running on your server, or find an alternate plugin.', 'pollux' );
+		$action = filter_input( INPUT_GET, 'action' );
+		$plugin = filter_input( INPUT_GET, 'plugin' );
 
-		printf( '<div id="message" class="notice notice-error error is-dismissible"><p><strong>%s</strong></p><p>%s %s</p></div>',
-			__( 'The Pollux plugin was deactivated.', 'pollux' ),
-			$message1,
-			$message2
+		if( $action == 'activate' ) {
+			$this->activatePlugin( $plugin );
+		}
+		else if( $action == 'install' ) {
+			$this->installPlugin( $plugin );
+		}
+		else if( filter_input( INPUT_GET, '_error_nonce' )) {
+			wp_safe_redirect( remove_query_arg( '_error_nonce' ));
+			exit;
+		}
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasDependency( $plugin )
+	{
+		if( !$this->isPluginDependency( $plugin )) {
+			return true;
+		}
+		return $this->isPluginInstalled( $plugin ) && $this->isPluginValid( $plugin );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasValidPHPVersion()
+	{
+		return version_compare( PHP_VERSION, self::MIN_PHP_VERSION, '>=' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasValidWPVersion()
+	{
+		global $wp_version;
+		return version_compare( $wp_version, self::MIN_WORDPRESS_VERSION, '>=' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isPluginActive( $plugin )
+	{
+		return $this->catchError( $plugin, 'inactive',
+			is_plugin_active( $plugin ) || array_key_exists( $plugin, $this->getMustUsePlugins() )
 		);
 	}
 
 	/**
 	 * @return bool
 	 */
-	public function proceed()
+	public function isPluginDependency( $plugin )
 	{
-		return $this->checkPhpVersion() && $this->checkWordPressVersion();
+		return array_key_exists( $plugin, static::DEPENDENCIES );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isPluginInstalled( $plugin )
+	{
+		return $this->catchError( $plugin, 'not_found',
+			array_key_exists( $plugin, $this->getAllPlugins() )
+		);
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isPluginValid( $plugin )
+	{
+		return $this->isPluginActive( $plugin ) && $this->isPluginVersionValid( $plugin );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isPluginVersionValid( $plugin )
+	{
+		if( !$this->isPluginDependency( $plugin )) {
+			return true;
+		}
+		if( !$this->isPluginInstalled( $plugin )) {
+			return false;
+		}
+		return $this->catchError( $plugin, 'wrong_version', version_compare(
+			$this->getPluginRequirements( $plugin, 'version' ),
+			$this->getAllPlugins()[$plugin]['Version'],
+			'<='
+		));
+	}
+
+	/**
+	 * @return void
+	 */
+	public function printNotices()
+	{
+		foreach( $this->notice->all as $notice ) {
+			echo $this->notice->generate( $notice );
+		}
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasPendingDependencies()
+	{
+		foreach( static::DEPENDENCIES as $plugin => $data ) {
+			if( !$this->isPluginDependency( $plugin ))continue;
+			$this->isPluginActive( $plugin );
+			$this->isPluginVersionValid( $plugin );
+		}
+		return !empty( $this->errors );
+	}
+
+	/**
+	 * @return void|null
+	 */
+	public function setDependencyNotice()
+	{
+		if( get_current_screen()->id != 'settings_page_pollux'
+			|| $this->app->config->disable_config
+			|| !$this->hasPendingDependencies()
+		)return;
+
+		$plugins = '';
+		$actions = '';
+
+		foreach( $this->errors as $plugin => $errors ) {
+			$plugins .= $this->getPluginLink( $plugin );
+			if( in_array( 'not_found', $errors ) && current_user_can( 'install_plugins' )) {
+				$actions .= $this->notice->installButton( $this->getPluginRequirements( $plugin ));
+			}
+			else if( in_array( 'wrong_version', $errors ) && current_user_can( 'update_plugins' )) {
+				$actions .= $this->notice->updateButton( $this->getPluginInformation( $plugin ));
+			}
+			else if( in_array( 'inactive', $errors ) && current_user_can( 'activate_plugins' )) {
+				$actions .= $this->notice->activateButton( $this->getPluginInformation( $plugin ));
+			}
+		}
+		$this->notice->addWarning([
+			sprintf( '<strong>%s</strong> %s', __( 'Pollux requires the latest version of the following plugins:', 'pollux' ), $plugins ),
+			$actions,
+		]);
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function addInvalidPHPVersionNotice()
+	{
+		$this->notice->addError([
+			$this->notice->title( __( 'The Pollux plugin was deactivated.', 'pollux' )),
+			sprintf( __( 'Sorry, Pollux requires PHP %s or greater in order to work properly (your server is running PHP %s).', 'pollux' ), self::MIN_PHP_VERSION, PHP_VERSION ),
+			__( 'Please contact your hosting provider or server administrator to upgrade the version of PHP running on your server, or find an alternate plugin.', 'pollux' ),
+		]);
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function addInvalidWPVersionNotice()
+	{
+		$this->notice->addError([
+			$this->notice->title( __( 'The Pollux plugin was deactivated.', 'pollux' )),
+			sprintf( __( 'Sorry, Pollux requires WordPress %s or greater in order to work properly.', 'pollux' ), self::MIN_WORDPRESS_VERSION ),
+			$this->notice->button( __( 'Update WordPress', 'pollux' ), self_admin_url( 'update-core.php' )),
+		]);
+	}
+
+	/**
+	 * @param string $error
+	 * @param bool $bool
+	 * @return bool
+	 */
+	protected function catchError( $plugin, $error, $isValid )
+	{
+		if( !$isValid ) {
+			if( !isset( $this->errors[$plugin] )) {
+				$this->errors[$plugin] = [];
+			}
+			$this->errors[$plugin] = array_keys( array_flip(
+				array_merge( $this->errors[$plugin], [$error] )
+			));
+		}
+		return $isValid;
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getAllPlugins()
+	{
+		require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		return array_merge( get_plugins(), $this->getMustUsePlugins() );
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getMustUsePlugins()
+	{
+		$plugins = get_mu_plugins();
+		if( in_array( 'Bedrock Autoloader', array_column( $plugins, 'Name' ))) {
+			$autoloadedPlugins = get_site_option( 'bedrock_autoloader' );
+			if( !empty( $autoloadedPlugins['plugins'] )) {
+				return array_merge( $plugins, $autoloadedPlugins['plugins'] );
+			}
+		}
+		return $plugins;
+	}
+
+	/**
+	 * @return array|false
+	 */
+	protected function getPlugin( $plugin )
+	{
+		if( $this->isPluginInstalled( $plugin )) {
+			return $this->getAllPlugins()[$plugin];
+		}
+		return false;
+	}
+
+	/**
+	 * @return array|string
+	 */
+	protected function getPluginData( $plugin, array $data, $key = null )
+	{
+		$data['plugin'] = $plugin;
+		$data['slug'] = $this->getPluginSlug( $plugin );
+		$data = array_change_key_case( $data );
+		if( is_null( $key )) {
+			return $data;
+		}
+		$key = strtolower( $key );
+		return isset( $data[$key] )
+			? $data[$key]
+			: '';
+	}
+
+	/**
+	 * @return array|string
+	 */
+	protected function getPluginInformation( $plugin, $key = null )
+	{
+		$data = $this->getPlugin( $plugin );
+		if( is_array( $data )) {
+			return $this->getPluginData( $plugin, $data, $key );
+		}
+		throw new Exception( sprintf( 'Plugin information not found for: %s', $plugin ));
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getPluginLink( $plugin )
+	{
+		try {
+			$data = $this->getPluginInformation( $plugin );
+		}
+		catch( Exception $e ) {
+			$data = $this->getPluginRequirements( $plugin );
+		}
+		return sprintf( '<span class="plugin-%s"><a href="%s">%s</a></span>',
+			$data['slug'],
+			$data['pluginuri'],
+			$data['name']
+		);
+	}
+
+	/**
+	 * @return array|string
+	 */
+	protected function getPluginRequirements( $plugin, $key = null )
+	{
+		$keys = ['Name', 'Version', 'PluginURI'];
+		$index = array_search( $key, $keys, true );
+		$requirements = $this->isPluginDependency( $plugin )
+			? array_pad( explode( '|', static::DEPENDENCIES[$plugin] ), 3, '' )
+			: array_fill( 0, 3, '' );
+		return $this->getPluginData( $plugin, array_combine( $keys, $requirements ), $key );
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getPluginSlug( $plugin )
+	{
+		return substr( $plugin, 0, strrpos( $plugin, '/' ));
 	}
 
 	/**
