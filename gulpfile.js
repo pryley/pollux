f6c8f5e284b59e323ed012a45260378fbf210fe0
Paul Ryley <paul@geminilabs.io>
2017-08-12 16:02:57 -0400

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var args            = require('yargs').argv;
 var autoprefixer    = require('gulp-autoprefixer');
-var babel           = require('gulp-babel');
 var bump            = require('gulp-bump');
 var checktextdomain = require('gulp-checktextdomain');
 var concat          = require('gulp-concat');
@@ -9,9 +8,9 @@ var gulp            = require('gulp');
 var gulpif          = require('gulp-if');
 var jshint          = require('gulp-jshint');
 var mergeStream     = require('merge-stream');
-var plumber         = require('gulp-plumber');
 var potomo          = require('gulp-potomo');
 var pseudo          = require('gulp-pseudo-i18n');
+var pump            = require('pump');
 var rename          = require('gulp-rename');
 var runSequence     = require('run-sequence');
 var sass            = require('gulp-sass');
@@ -25,14 +24,12 @@ var config = yaml.load('+/config.yml');
 /* JSHint Task
  -------------------------------------------------- */
 gulp.task('jshint', function() {
-  return gulp.src(config.watch.js)
-  .pipe(plumber({
-    errorHandler: function(error) {
-    console.log(error.message);
-    this.emit('end');
-  }}))
-  .pipe(jshint())
-  .pipe(jshint.reporter('jshint-stylish'))
+  pump([
+    gulp.src(config.watch.js),
+    jshint(),
+    jshint.reporter('jshint-stylish'),
+    jshint.reporter('fail'),
+  ]);
 });
 
 /* JS Task
@@ -42,19 +39,13 @@ gulp.task('js', function() {
   for(var key in config.scripts) {
     streams.add(gulp.src(config.scripts[key]).pipe(concat(key)));
   }
-  return streams
-  .pipe(plumber({
-    errorHandler: function(error) {
-    console.log(error.message);
-    this.emit('end');
-  }}))
-  // .pipe(babel({
-  //   presets: ["env"]
-  // }))
-  .pipe(gulpif(args.production, uglify({
-    preserveComments: 'license',
-  })))
-  .pipe(gulp.dest(config.dest.js))
+  pump([
+    streams,
+    gulpif(args.production, uglify({
+      output: { comments: 'some' },
+    })),
+    gulp.dest(config.dest.js),
+  ]);
 });
 
 /* CSS Task
@@ -64,34 +55,28 @@ gulp.task('css', function() {
   for(var key in config.styles) {
     streams.add(gulp.src(config.styles[key]).pipe(concat(key)));
   }
-  return streams
-  .pipe(plumber({
-    errorHandler: function(error) {
-    console.log(error.message);
-    this.emit('end');
-  }}))
-  .pipe(gulpif(args.production, cssnano()))
-  .pipe(gulp.dest(config.dest.css))
+  pump([
+    streams,
+    gulpif(args.production, cssnano()),
+    gulp.dest(config.dest.css),
+  ]);
 });
 
 /* SCSS Task
  -------------------------------------------------- */
 gulp.task('scss', function() {
-  return gulp.src(config.watch.scss)
-  .pipe(plumber({
-    errorHandler: function(error) {
-    console.log(error.message);
-    this.emit('end');
-  }}))
-  .pipe(sass({
-    outputStyle: 'expanded',
-  }))
-  .pipe(autoprefixer('last 2 versions'))
-  .pipe(gulpif(args.production, cssnano({
-    minifyFontValues: false,
-    discardComments: { removeAll: true }
-  })))
-  .pipe(gulp.dest(config.dest.css))
+  pump([
+    gulp.src(config.watch.scss),
+    sass({
+      outputStyle: 'expanded',
+    }).on('error', sass.logError),
+    autoprefixer('last 2 versions'),
+    gulpif(args.production, cssnano({
+      minifyFontValues: false,
+      discardComments: { removeAll: true }
+    })),
+    gulp.dest(config.dest.css),
+  ]);
 });
 
 /* Language Tasks
@@ -101,44 +86,47 @@ gulp.task('languages', function() {
 });
 
 gulp.task('po', function() {
-  return gulp.src(config.watch.php)
-  .pipe(checktextdomain({
-    text_domain: config.language.domain,
-    keywords: [
-      '__:1,2d',
-      '_e:1,2d',
-      '_x:1,2c,3d',
-      'esc_html__:1,2d',
-      'esc_html_e:1,2d',
-      'esc_html_x:1,2c,3d',
-      'esc_attr__:1,2d',
-      'esc_attr_e:1,2d',
-      'esc_attr_x:1,2c,3d',
-      '_ex:1,2c,3d',
-      '_n:1,2,4d',
-      '_nx:1,2,4c,5d',
-      '_n_noop:1,2,3d',
-      '_nx_noop:1,2,3c,4d',
-    ],
-  }))
-  .pipe(sort())
-  .pipe(wpPot({
-    domain: config.language.domain,
-    lastTranslator: config.language.translator,
-    team: config.language.team,
-  }))
-  .pipe(pseudo({
-    // language: 'en_US',
-    charMap: {},
-  }))
-  .pipe(rename(config.language.domain + '-en_US.po'))
-  .pipe(gulp.dest(config.dest.lang));
+  pump([
+    gulp.src(config.watch.php),
+    checktextdomain({
+      text_domain: config.language.domain,
+      keywords: [
+        '__:1,2d',
+        '_e:1,2d',
+        '_x:1,2c,3d',
+        'esc_html__:1,2d',
+        'esc_html_e:1,2d',
+        'esc_html_x:1,2c,3d',
+        'esc_attr__:1,2d',
+        'esc_attr_e:1,2d',
+        'esc_attr_x:1,2c,3d',
+        '_ex:1,2c,3d',
+        '_n:1,2,4d',
+        '_nx:1,2,4c,5d',
+        '_n_noop:1,2,3d',
+        '_nx_noop:1,2,3c,4d',
+      ],
+    }),
+    sort(),
+    wpPot({
+      domain: config.language.domain,
+      lastTranslator: config.language.translator,
+      team: config.language.team,
+    }),
+    pseudo({
+      charMap: {},
+    }),
+    rename(config.language.domain + '-en_US.po'),
+    gulp.dest(config.dest.lang),
+  ]);
 });
 
 gulp.task('mo', function() {
-  return gulp.src(config.dest.lang + '*.po')
-  .pipe(potomo())
-  .pipe(gulp.dest(config.dest.lang));
+  pump([
+    gulp.src(config.dest.lang + '*.po'),
+    potomo(),
+    gulp.dest(config.dest.lang),
+  ]);
 });
 
 /* Version Task
@@ -147,12 +135,13 @@ gulp.task('bump', function() {
   ['patch', 'minor', 'major'].some(function(arg) {
     if(!args[arg])return;
     for(var key in config.bump) {
-      gulp.src(config.bump[key]).pipe(bump({
-        type: arg,
-        key: key,
-      })).pipe(gulp.dest('.'));
+      if(!config.bump.hasOwnProperty(key))continue;
+      pump([
+        gulp.src(config.bump[key]),
+        bump({type:arg,key:key}),
+        gulp.dest('.'),
+      ]);
     }
-    return true;
   });
 });
 
