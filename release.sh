725177fd2f1bf08673d9f91afcd7f89455364f83
Paul Ryley <paul@geminilabs.io>
2018-05-26 15:57:57 +0400

    fix release script

--- release.sh
+++ release.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # By Paul Ryley, based on work by Mike Jolley
 # License: GPLv3
+# Version: 1.0.1
 
 # ----- START EDITING HERE -----
 
+ASSETS_DIR="+/assets"
 DEFAULT_GIT_BRANCH="master"
 MIN_PHP_VERSION="5.6"
 MIN_WORDPRESS_VERSION="4.7"
@@ -46,6 +48,7 @@ read -p " - Verified compatibility with Wordpress v${MIN_WORDPRESS_VERSION} -> l
 read -p " - Updated the POT file?"
 read -p " - Updated the screenshots?"
 read -p " - Committed all changes to the master branch on GITHUB?"
+read -p " - Scrutinizer has passed all inspections?"
 echo ""
 read -p "PRESS [ENTER] TO BEGIN RELEASING "${PLUGIN_VERSION}
 clear
@@ -57,7 +60,9 @@ rm -Rf $ROOT_PATH$TEMP_GITHUB_REPO
 if [[ ! -d $SVN_REPO_DIR ]]; then
 	echo "Checking out WordPress.org plugin repository"
 	mkdir -p $ROOT_PATH$SVN_REPO_DIR
-	svn checkout $SVN_REPO $SVN_REPO_DIR || { echo "Unable to checkout repo."; exit 1; }
+	cd $ROOT_PATH$SVN_REPO_DIR
+	svn checkout $SVN_REPO . || { echo "Unable to checkout repo."; exit 1; }
+	cd $ROOT_PATH
 fi
 
 # LIST BRANCHES
@@ -81,7 +86,7 @@ read -p "PRESS [ENTER] TO DEPLOY BRANCH "${BRANCH:-$DEFAULT_GIT_BRANCH}
 cd $ROOT_PATH$SVN_REPO_DIR
 
 # COPY ASSETS to SVN DIR
-cp $ROOT_PATH/+/assets/* ./assets/
+cp $ROOT_PATH/$ASSETS_DIR/* $ROOT_PATH$SVN_REPO_DIR/assets/
 
 # UPDATE SVN
 echo "Updating SVN"
@@ -89,10 +94,10 @@ svn update || { echo "Unable to update SVN."; exit 1; }
 
 # DELETE TRUNK
 echo "Replacing trunk"
-rm -Rf trunk/
+rm -Rf $ROOT_PATH$SVN_REPO_DIR/trunk/
 
 # COPY GIT DIR TO TRUNK
-cp -R $ROOT_PATH$TEMP_GITHUB_REPO trunk/
+cp -R $ROOT_PATH$TEMP_GITHUB_REPO $ROOT_PATH$SVN_REPO_DIR/trunk/
 
 # DO THE ADD ALL NOT KNOWN FILES UNIX COMMAND
 svn add --force * --auto-props --parents --depth infinity -q
@@ -124,7 +129,7 @@ echo ""
 echo "Committing to WordPress.org...this may take a while."
 svn commit -m "Release "${PLUGIN_VERSION}", see readme.txt for the changelog." || { echo "Unable to commit."; exit 1; }
 
-# REMOVE THE TEMP DIR
+# REMOVE THE TEMP DIRS
 echo "CLEANING UP"
 rm -Rf $ROOT_PATH$TEMP_GITHUB_REPO
 
